5dda9cf0817c9324046406f7e3f11fd6b9f06e74
fendijatmikon <jatmikofendi@gmail.com>
2018-06-09 21:41:12 +0700

    add init config

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # nessaGo
-yoben
--- cmd/root.go
+++ cmd/root.go
@@ -32,3 +32,36 @@ var (
 	schemaQuestion, schemaValidate, schemaVerify *graphql.Schema
 	templates                                    *template.Template
 )
+
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig, initLogger)
+	RootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "config file (default is $HOME/.gareng.toml)")
+	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func initConfig() {
+	viper.SetConfigType("toml")
+
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		home, err := homedir.Dir()
+		if err != nil {
+			panic(err)
+		}
+		viper.AddConfigPath(".")
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".gareng")
+	}
+	viper.AutomaticEnv()
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
